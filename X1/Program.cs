8c51c1c08a9a45573533e9eae6531fae75dba1c0
XUEkfk <love.kyngh.ky@gmail.com>
2024-08-23 10:35:12 +0800

    第一次測試

---
+++ X1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
