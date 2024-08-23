d32c150a9c964cdedcaca650b3745aa33c5880c4
XUEkfk <love.kyngh.ky@gmail.com>
2024-08-23 11:54:45 +0800

    主宇宙之後

--- X1/Program.cs
+++ X1/Program.cs
@@ -10,6 +10,7 @@ namespace X1
             Console.WriteLine("1234");
             Console.WriteLine("3+2");
             Console.WriteLine("不小心刪掉主宇宙了ㄟ黑OVO");
+            Console.WriteLine("刪掉主宇宙的駔魁禍首OVO");
         }
     }
 }
